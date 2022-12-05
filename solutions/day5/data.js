afdc71de7c3ac4a265aeaa9c50c719aefc94e1ef
alicialowe <hohpascal@outlook.com>
2022-12-05 21:03:04 +0000

    solution to Day 5 - Candy Sale

---
+++ solutions/day5/data.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        item: "🍭",
+        price: 2.99,
+        type: "sweet",
+    },
+    {
+        item: "🍫",
+        price: 1.99, 
+        type: "sweet",
+    },
+    {
+        item: "🥫",
+        price: 1.99, 
+        type: "savory",
+    },
+    {
+        item: "🍬",
+        price: .89, 
+        type: "sweet",
+    },
+    {
+        item: "🥦",
+        price: 3.99,
+        type: "savory",
+    }, 
+    {
+        item: "🍖",
+        price: 3.99, 
+        type: "savory",
+    }, 
+]
+
+export default products;
