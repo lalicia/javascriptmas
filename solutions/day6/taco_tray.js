e712adf1212de69e5ff9ba5847aeeb8b3e2b010e
alicialowe <hohpascal@outlook.com>
2022-12-06 12:46:36 +0000

    solution to Day 6 - Taco Tray

---
+++ solutions/day6/taco_tray.js
@@ -0,0 +1,26 @@
+// Review Challenge 4: Taco Tray
+// - Help our chef fill a tray with tacos! 
+
+function getRandomNumberOfTacos() {
+    /*
+    Make this function return an array that contains 
+    between one and ten taco emojis 🌮
+    Use the following JavaScript concepts:
+        - Math.random()
+        - Math.floor()
+        - new Array()
+        - Array.fill()
+    */
+    
+    //setting the size of the array with Math.floor and Math.random, then filling each index with a taco
+    return new Array(Math.floor(Math.random() * 11)).fill("🌮"); // replace this empty tray array
+
+}
+
+function putTacosOnTray() {
+    return getRandomNumberOfTacos().map(function (taco) {
+        return `<div class="taco">${taco}</div>`
+    }).join('')
+}
+
+document.getElementById('tray').innerHTML = putTacosOnTray()
